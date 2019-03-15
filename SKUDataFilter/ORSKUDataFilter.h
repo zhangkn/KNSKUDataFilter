4e2688df9ac2310da2b33e4c7256960d03fc3031
zhangkn <929118967@qq.com>
2019-03-15 15:07:22 +0800

    #warning filter:propertiesInSection：和方法filter:conditionForRow：数据类型应该保持一致  这里不关心具体的值，可以是属性ID, 属性名，字典、model

--- SKUDataFilter/ORSKUDataFilter.h
+++ SKUDataFilter/ORSKUDataFilter.h
@@ -1,11 +1,7 @@
 //
 //  ORSKUDataFilter.h
 //  SKUFilterfilter
-//
-//  Created by OrangesAL on 2017/12/3.
-//  Copyright © 2017年 OrangesAL. All rights reserved.
-//
-
+#warning filter:propertiesInSection：和方法filter:conditionForRow：数据类型应该保持一致  这里不关心具体的值，可以是属性ID, 属性名，字典、model
 /* Tips：
  *
  *  ----------------- warning -----------------
